370ca71b931dbfe047b06f5b4c543e3729f35484
Sunita <sunitav@gmail.com>
2021-08-13 15:50:03 -0700

    Add files

---
+++ FlashFlowerClassification.py
@@ -0,0 +1,35 @@
+import flash
+from flash.core.data.utils import download_data
+from flash.image import ImageClassificationData, ImageClassifier
+
+# 1. Use the datastore 
+
+datamodule = ImageClassificationData.from_folders(
+    train_folder="/home/jovyan/mlproject/FlowerClassification/flower-dataset/train/data/hymenoptera_data/train/",
+    val_folder="/home/jovyan/mlproject/FlowerClassification/flower-dataset/val/",
+    test_folder="/home/jovyan/mlproject/FlowerClassification/flower-dataset/test/",
+)
+
+# 2. Build the task
+model = ImageClassifier(num_classes=datamodule.num_classes)
+
+# 3. Create the trainer and finetune the model
+trainer = flash.Trainer(max_epochs=3)
+trainer.finetune(model, datamodule=datamodule, strategy="freeze_unfreeze")
+ 
+trainer.test()
+
+
+# 4. Predict what's on a few images! ants or bees?
+model.serializer = Labels()
+
+predictions = model.predict(
+    [
+        "/home/jovyan/mlproject/FlowerClassification/flower-dataset/val/roses/4644336779_acd973528c.jpg",
+        "/home/jovyan/mlproject/FlowerClassification/flower-dataset/val/tulips/2436998042_4906ea07af.jpg",
+    ]
+)
+print(predictions)
+
+# 5. Save the model!
+trainer.save_checkpoint("image_classification_model.pt")
